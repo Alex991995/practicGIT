3e2928a3b6f17fb5ac51f0d0062637ed39855aa3
Sasha <example@gmail.com>
2019-08-16 19:57:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// 'use strict';
+
+a=5;
+console.log(a);
+
+
+
+console.log(4/0);
+console.log('string'*8 );
+let log;
+console.log(log);
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+console.log(persone['name']);
+
+let arr=['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+// alert('Hello world');
+
+// let answer = confirm("Ты здесь?");
+
+// // console.log(answer);
+// let answer = +prompt("Есть ли вам 18?","Да");
+
+// console.log(typeof(answer));
+
+// // console.log("arr" + " - object");
+// console.log(5 + + " - object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2"=== 2);
+
+let isCheked = false;
+    isClose = false;
+
+    console.log(isCheked || !isClose);
